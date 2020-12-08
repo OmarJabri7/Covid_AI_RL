efdc6cf1c1efc23da06b40c7d79eb641d59027bd
Omar Jabri <35663665+OmarJabri7@users.noreply.github.com>
2020-12-08 17:10:48 +0000

    added some comments for clarity once run

--- run_eval.py
+++ run_eval.py
@@ -7,8 +7,22 @@ import matplotlib.pyplot as plt
 plt.ioff() #remove if you want to interactively see the grahs (will be a lot of graphs)
 #evaluate performance of differebt agent and save graphs in the directories, if not exist, the programs will create them.
 
-# evaluate_random(True,True) #Random/
-# evaluate_deterministic_model(False,True) #Deterministic/
-# rewards_q_tabular = evaluate_q_tabular(500,True,True,True,True) #Q_tabular/
-rewards_q_nn = evaluate_q_nn(250,False,True,False,True) #Q_nn_plots/
-# rewards_policy_nn = evaluate_policy_search(2000) #Policy_Search/
+evaluate_random(stochastic = True, noisy = True) #Random/
+
+evaluate_deterministic_model(stochastic = False,noisy = True) #Deterministic/
+
+rewards_q_tabular = evaluate_q_tabular(500,stochastic_train = False,
+                                       noisy_train = False,
+                                       stochastic_test = False,
+                                       noisy_test = False) #Q_tabular/
+rewards_q_nn = evaluate_q_nn(250,
+                             stochastic_train=False,
+                             noisy_train=True,
+                             stochastic_test=False,
+                             noisy_test=True) #Q_nn_plots/
+
+rewards_policy_nn = evaluate_policy_search(2000,
+                                           stochastic_train=False,
+                                           noisy_train=False,
+                                           stochastic_test=False,
+                                           noisy_test=False) #Policy_Search/
